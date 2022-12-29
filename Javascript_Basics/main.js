03e0a35d4e33aff118a95bb1c1eb95b1cc9e911a
Siva9727 <sivasakthi203023@gmail.com>
2022-12-29 20:31:55 +0530

    js basics

--- Javascript_Basics/main.js
+++ Javascript_Basics/main.js
@@ -1,18 +1,28 @@
-console.log("hello world")
-console.log("hello to web dev")
-//single line comment in js
-/* multi line 
-comments in js */
-console.log(5+6)
-// declare variables in js
-var fname ="siva";
-console.log(fname);
-var first_name = prompt('enter your name?')
-console.log(first_name);
-var age = prompt('enter your age?');
-var result = age > 18;
-if (result) {
-    alert('eligible')
-} else {
-    alert('not eligible')
+// console.log("hello world")
+// console.log("hello to web dev")
+// //single line comment in js
+// /* multi line 
+// comments in js */
+// console.log(5+6)
+// // declare variables in js
+// var fname ="siva";
+// console.log(fname);
+// var first_name = prompt('enter your name?')
+// console.log(first_name);
+// var age = prompt('enter your age?');
+// var result = age > 18;
+// if (result) {
+//     alert('eligible')
+// } else {
+//     alert('not eligible')
+// }
+// var i = 0;
+// while (i<4) {
+//     console.log(i);
+//     i++;
+// }
+
+function print() {
+    console.log("hello, world!");
 }
+print();

12423c21cb6134b78a04429177d85326be4b4132
Siva9727 <sivasakthi203023@gmail.com>
2022-12-30 00:40:50 +0530

    hoisting,DOM and BOM

---
+++ Javascript_Basics/declare.js
@@ -0,0 +1,19 @@
+//var: reinitialize and redeclare the values 
+var a = 10;
+console.log(a);
+a = 20;
+console.log(a);
+var a = 30;
+console.log(a);
+//let: reinitialize but can't redeclare
+let b = 10;
+console.log(b);
+b = 20;
+console.log(b);
+// let b = 30;
+// console.log(b);
+
+
+// const: we can't reinitialize and redeclare const
+const PI = 3.14;
+console.log(PI);
---
+++ Javascript_Basics/dom.js
@@ -0,0 +1,9 @@
+let paragraph = document.getElementById("para");
+console.log(paragraph);
+//to get text 
+console.log(paragraph.textContent);
+
+document.querySelector("h1")// second method using query selector
+
+// third method 
+document.querySelectorAll("h1")

c6cc4063f3fcc8750bfbb8ee5c07c785b1af820f
Siva9727 <sivasakthi203023@gmail.com>
2023-01-13 00:04:11 +0530

    event listener

--- Javascript_Basics/DiceGame/script.js
+++ Javascript_Basics/DiceGame/script.js
@@ -3,9 +3,9 @@ let playerName = document.getElementsByTagName("input");
 //its the faster way using getElement method
 
 //second way is using query selector. It's one of the powerful tool but a bit slower
-let name = document.querySelector("input");  
+let name = document.querySelector("input");
 // select the first input tag-- return the element 
-let nameAll = document.querySelectorAll("input"); 
+let nameAll = document.querySelectorAll("input");
 // select all iput tag --  return answer in form an array 
 
 // innerText, innerHTML
@@ -17,3 +17,21 @@ let score = document.getElementsByTagName("span");
 
 //event--> any action on the website
 //addEventListener  --> method to check whether an action is occured
+
+let player1 = nameAll[0];
+let btns1 = btns[0];
+let score1 = score[0];
+
+//addEventListener
+btns1.addEventListener("click", () => { rollDice("hi") }) //rollDice is a callback, that means somewhere there is a function called rollDice.
+
+// function rollDice() {
+//     console.log("button clicked");
+// }
+
+function rollDice(x) {
+    console.log(x);
+    console.log("button clicked");
+}
+// two ways you can pass the fuction, one by passing the function name only{ here , rollDice not like rollDice}, 
+//2. in order to provide the argument use the anonymous function

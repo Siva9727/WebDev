98e73a68a32aeb363934121b5591fd35cf43b7de
Siva9727 <sivasakthi203023@gmail.com>
2023-01-12 23:33:41 +0530

    callback

---
+++ Javascript_Basics/DiceGame/callback.js
@@ -0,0 +1,54 @@
+
+function hello(x,fun1) {
+    console.log("hello",x);
+    fun1();
+    // console.log(fun1);
+
+}
+
+//Normal function
+// function info() {
+//     console.log("hello, im siva");
+// }
+
+// let a = 10;
+
+// hello(a,info)
+
+
+// Arrow function:
+// let info1 = (a)=> {
+//     console.log("hello, im siva",a);
+// }
+// info1(10)
+
+// let sum = (a,b) => {
+//     console.log(a+b);
+// }
+// sum(10,20);
+
+// No name or Anonymous function
+// function (a,b){
+//     console.log(a+b);
+// }
+
+let a = 10;
+let two = (value) => {
+    console.log("my name is sivasakthi",value);
+}
+
+// hello(a,two) // normal calling 
+// let x = two;
+// let y = two();
+// console.log(y);
+
+//passing values 
+
+// hello(10,two())
+//you can use no name function here for passing the value 
+
+// hello(10, () => {console.log("my name is sivasakthi");})
+
+// to pass the paramaterized function as parameter --> use no name function
+hello(10, () => {two(20)})
+
--- Javascript_Basics/DiceGame/script.js
+++ Javascript_Basics/DiceGame/script.js
@@ -3,5 +3,17 @@ let playerName = document.getElementsByTagName("input");
 //its the faster way using getElement method
 
 //second way is using query selector. It's one of the powerful tool but a bit slower
-let name = document.querySelector("input");  // select the first input tag
-let nameAll = document.querySelectorAll("input"); // select all iput tag
+let name = document.querySelector("input");  
+// select the first input tag-- return the element 
+let nameAll = document.querySelectorAll("input"); 
+// select all iput tag --  return answer in form an array 
+
+// innerText, innerHTML
+/*
+InnerHTML --> returns the content in between the opening and closing tag.eg{h1}
+*/
+let btns = document.getElementsByClassName("players");
+let score = document.getElementsByTagName("span");
+
+//event--> any action on the website
+//addEventListener  --> method to check whether an action is occured

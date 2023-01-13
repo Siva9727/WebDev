c43e187f84920736105b586464262dc5957519d8
Siva9727 <sivasakthi203023@gmail.com>
2023-01-14 00:22:13 +0530

    hoisting

---
+++ Javascript_Basics/function/ScopeFunction.js
@@ -0,0 +1,16 @@
+function sum() {
+    let a = 10 
+    // let is block scope 
+    var b = 20;
+    // var is global scope except in function
+    c=30 //its global variable, js will declare the variable automatically.
+}
+
+sum();
+// console.log(d);
+// console.log(a);
+console.log(c);
+d=50; // outside the function scope, it act as let 
+console.log(d);
+
+// hositing happens to var, functions with normal syntax, class
--- Javascript_Basics/function/scope.js
+++ Javascript_Basics/function/scope.js
@@ -5,8 +5,33 @@
 
 let x = 10; // global variable
 
-if(true){
-    let y = 20;  // local variable
-}
-console.log(x);
+// if(true){
+//     let y = 20;  // local variable
+// }
+// console.log(x);
 // console.log(y);
+
+// Normally you can access global variable anywhere except in local scope the same variable is reinitialized
+for (let i = 0; i < 1; i++) {
+    // console.log(x);    this statement throws an error and proves our above statement 
+    let x = 50
+    console.log( "inside",x);
+}
+console.log("outside",x);
+
+/*         <---------------------- for VAR  -------------------------> "HOISTING"                    */
+
+// function hello() {
+//     console.log("hello");
+// }
+// console.log(a);
+
+// var a = 10; // automatically the the var declaration goes to the top part or global scope
+// console.log(a);
+
+console.log(a);
+if (true) {
+    // still var declaration moves to the global scope 
+    var a = 10;
+}
+console.log(a);

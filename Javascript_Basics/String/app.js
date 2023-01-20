3d0f4cc0e6db9c63ffe431c5f3f8b1fc44d0376e
Siva9727 <sivasakthi203023@gmail.com>
2023-01-20 23:39:19 +0530

    string in js

---
+++ Javascript_Basics/String/app.js
@@ -0,0 +1,12 @@
+
+let myString ="Hello, World";
+// // convert lowercase
+// let a = myString.toLowerCase();
+// // to convert given string to upper case
+// let b = myString.toUpperCase();
+
+
+let myStringUpper = myString.toUpperCase(); // "HELLO, WORLD!"
+let myStringLower = myString.toLowerCase(); // "hello, world!"
+let myStringSub = myString.substring(0, 5); // "Hello"
+let myStringReplace = myString.replace("World", "Universe"); // "Hello, Universe!"
---
+++ Javascript_Basics/String/basics.js
@@ -0,0 +1,8 @@
+let firstName = "John";
+let lastName = "Doe";
+// concate string 
+let fullName = firstName + " " + lastName; // "John Doe"
+console.log(fullName);
+
+// access character 
+console.log(fullName[fullName.length-1]);

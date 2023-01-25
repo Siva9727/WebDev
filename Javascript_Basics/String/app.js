10ccffb3dccf0fa5effc79579fb2f1fa34aaec5e
Siva9727 <sivasakthi203023@gmail.com>
2023-01-25 22:15:36 +0530

    car game html added

--- Javascript_Basics/String/app.js
+++ Javascript_Basics/String/app.js
@@ -1,5 +1,5 @@
 
-let myString ="Hello, World";
+let myString = "Hello, World";
 // // convert lowercase
 // let a = myString.toLowerCase();
 // // to convert given string to upper case
@@ -10,3 +10,9 @@ let myStringUpper = myString.toUpperCase(); // "HELLO, WORLD!"
 let myStringLower = myString.toLowerCase(); // "hello, world!"
 let myStringSub = myString.substring(0, 5); // "Hello"
 let myStringReplace = myString.replace("World", "Universe"); // "Hello, Universe!"
+
+/*
+.slice(beginIndex, endIndex) : This method returns a new string containing the extracted portion of the given string.
+ Similar to .substring(), but it can also handle negative indices. 
+*/
+let sliceString = myString.slice(7,13); // "World!"

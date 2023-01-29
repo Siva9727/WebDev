a85a1fd7933ca8c90d94025ded23c6a3b3168d6b
Siva9727 <sivasakthi203023@gmail.com>
2023-01-29 19:49:59 +0530

    modified

---
+++ Javascript_Basics/CarGame/app.js
@@ -0,0 +1,10 @@
+let keys = {
+    ArrowUp:false,
+    ArrowDown:false,
+    ArrowRight:false,
+    ArrowLeft:false
+}
+
+for (const iterator in keys) {
+    console.log(iterator,keys[iterator]);
+}

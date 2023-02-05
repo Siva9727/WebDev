5661ea74f909b1659ee7e2ec675538368dd7cb85
Siva9727 <sivasakthi203023@gmail.com>
2023-02-05 21:46:36 +0530

    modified

--- Javascript_Basics/basics/dom.js
+++ Javascript_Basics/basics/dom.js
@@ -1,13 +1,18 @@
-let paragraph = document.getElementById("para");
-console.log(paragraph);
-//to get text 
-console.log(paragraph.textContent);
+// let paragraph = document.getElementById("para");
+// console.log(paragraph);
+// //to get text 
+// console.log(paragraph.textContent);
 
-document.querySelector("h1")// second method using query selector
+// document.querySelector("h1")// second method using query selector
 
-// third method 
-document.querySelectorAll("h1")
-let element = document.getElementsByTagName('h1');
-console.log(paragraph.innerHTML);
-paragraph.innerHTML='changed using innerHTML';
-paragraph.innerText='changed using innerText';
+// // third method 
+// document.querySelectorAll("h1")
+// let element = document.getElementsByTagName('h1');
+// console.log(paragraph.innerHTML);
+// paragraph.innerHTML='changed using innerHTML';
+// paragraph.innerText='changed using innerText';
+
+//ternary operator 
+let age = 19;
+let a  = age>18 ? "You can drive" : "You cannot drive" 
+console.log(a)

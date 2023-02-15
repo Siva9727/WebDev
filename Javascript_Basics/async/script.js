def0955ebe8229eec00e76052944fdfeb3f6fc79
Siva9727 <sivasakthi203023@gmail.com>
2023-02-15 23:50:04 +0530

    async js

---
+++ Javascript_Basics/async/script.js
@@ -0,0 +1,14 @@
+
+
+console.log("hello")
+// function set(callback,time){
+//     callback(time);
+// }
+
+// function hello(t) {
+//     setTimeout(() => {
+//         console.log("Hello everyone after 5 sec")
+//     }, t*1000);
+// }
+
+// set(hello,5);
